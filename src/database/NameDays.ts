0ec5b5defaf046b37344a4f97d9f57f3bfe89ad6
Tomas Bruckner <tomasbrucknermail@gmail.com>
2018-05-23 09:59:27 +0200

    Data folder

--- src/database/NameDays.ts
+++ src/database/NameDays.ts
@@ -2,7 +2,7 @@ import { NameDay } from "../common/types";
 import Database from "./Database";
 
 class NameDays extends Database {
-    protected path: string = "./nameDays.json";
+    protected path: string = "../../data/nameDays.json";
 
     public getNameDay(date: string): NameDay {
         return this.data[date];
--- src/database/PublicHolidays.ts
+++ src/database/PublicHolidays.ts
@@ -2,7 +2,7 @@ import { PublicHoliday } from "../common/interfaces";
 import Database from "./Database";
 
 class PublicHolidays extends Database {
-    protected path: string = "./publicHolidays.json";
+    protected path: string = "../../data/publicHolidays.json";
 
     public getPublicHoliday(date: string): PublicHoliday {
         return this.data[date];
